015734f19df7a00ddd62ff89993868352aeebff9
GiteaBot <teabot@gitea.io>
2025-11-16 00:01:17 +0000

    [skip ci] Updated Version via cron

--- act_runner.rb
+++ act_runner.rb
@@ -3,7 +3,7 @@ require_relative './utils/macos_codesign.rb'
 class ActRunner < Formula
   desc "A runner for Gitea based on act"
   homepage "https://gitea.com/gitea/act_runner"
-  version "0.2.11"
+  version "0.2.13"
   license "MIT"
 
   os = OS.mac? ? "darwin" : "linux"
@@ -22,10 +22,10 @@ class ActRunner < Formula
   depends_on "xz"
 
   @@sha256 = case "#{os}-#{arch}"
-             when "linux-amd64" then "6099bccc2876cb760f5443e88c8b2e1a52f226f485152fc6f711b3bb3f49501c"
-             when "linux-arm64" then "e155c3a476921b6636947af90c7119758c9332c5b82c4a15ed86e17c4d7204e7" # binary
-             when "darwin-amd64" then "df885b72c844378aa33369738d35b147c8ac6b31d6f2a6d303395208d2e9e41b"
-             when "darwin-arm64" then "b4169b914d9ff8c2014289498872389310c79ecf73d41ac132ae4820955240a4"
+             when "linux-amd64" then "759821f7afc40afb9d0e55c07b7832e0fa08374e0f2b9626516156903cd632ad"
+             when "linux-arm64" then "20a97de2236232fdf72c11a87fc0f6f4747a31f322e7ca7e63df83ebc17e6bf7" # binary
+             when "darwin-amd64" then "6099d7969107717fa6d35fea2c7ae0999acd300124f48516d215c2bbbd812cfa"
+             when "darwin-arm64" then "0a2d898b46c413217f6f6b16662a5aa9618e7c6bfcd62aca272db2074450c547"
              else
                raise "act_runner: Unsupported system #{os}-#{arch}"
              end
